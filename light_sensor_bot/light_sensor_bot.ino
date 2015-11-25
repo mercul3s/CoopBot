13b943db43551784d15ee3d0aec5e376a771f870
Mercedes Coyle <mcoyle@ulive.com>
2015-11-24 23:07:04 -0800

    Added RGB LED with color to indicate temp ranges

--- light_sensor_bot/light_sensor_bot.ino
+++ light_sensor_bot/light_sensor_bot.ino
@@ -1,7 +1,11 @@
 // Sensor pins 
 const int lightSensorPin = 0;
 const int tempSensorPin = 1;
-const int ledPin = 9;
+
+// LED pins for temperature visualization
+const int redLedPin = 9;
+const int grnLedPin = 10;
+const int bluLedPin = 11;
 
 // Variables for sensor readings
 int lightLevel, high = 1023, low = 0;
@@ -35,11 +39,6 @@ void sendValues() {
   // nothing to see here
 }
 
-void setup() {
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
-}
-
 float getTempSensorVoltage(int pin) {
   return (analogRead(pin) * 0.004882814);
 }
@@ -56,12 +55,72 @@ void checkLastTempVal(float currentTempF) {
    }
 }
 
+void tempColor(float temperature) {
+
+  if (temperature >= 95.0) {
+    // Red
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(grnLedPin, LOW);
+    digitalWrite(bluLedPin, LOW);
+   
+  }
+  else if (temperature <= 94.99 && temperature > 80.00) {
+    // Orange/Yellow
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(grnLedPin, HIGH);
+    digitalWrite(bluLedPin, LOW);
+    
+  }
+  else if (temperature <= 79.99 and temperature > 60.00) {
+    // Green
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(grnLedPin, HIGH);
+    digitalWrite(bluLedPin, LOW);
+    
+  }
+  else if (temperature <= 59.99 and temperature > 40.00) {
+    // Purple (turn red and blue on):
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(grnLedPin, LOW);
+    digitalWrite(bluLedPin, HIGH);
+  }
+  else if (temperature <= 39.99 and temperature > 32.50) {
+    // Blue
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(grnLedPin, LOW);
+    digitalWrite(bluLedPin, HIGH);
+  }
+  else if (temperature <= 32.49) {
+    // White
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(grnLedPin, HIGH);
+    digitalWrite(bluLedPin, HIGH);
+  }
+  else {
+    // turn all off
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(grnLedPin, LOW);
+    digitalWrite(bluLedPin, LOW);
+  }
+}
+
+void setup() {
+  pinMode(redLedPin, OUTPUT);
+  pinMode(grnLedPin, OUTPUT);
+  pinMode(bluLedPin, OUTPUT);
+  Serial.begin(9600);
+}
+
 void loop() {
   // Read photo sensor every minute, and format the value for LED output.
+  // If there is a change in temp greater than 1 degree, set it to lastTempF
+  // Log this value later.
   // Need to get a good baseline of light/dark values for opening coop door
   lightLevel = analogRead(lightSensorPin);
   tempLevel = getTempSensorVoltage(tempSensorPin);
   degreesF = formatTempF(tempLevel);
+  tempColor(degreesF);
+  checkLastTempVal(degreesF);
   
   // loop timing in arduino: at the start of your loop (sensor read), you
   // set a timer to be milliseconds, and it starts counting down for you.
@@ -76,7 +135,6 @@ void loop() {
   Serial.println(degreesF);
   
   lightLevel = tuneLightLevel(lightLevel);
-  analogWrite(ledPin, lightLevel);
   delay(10000);
 }
 
