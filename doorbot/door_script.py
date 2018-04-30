09b6d8bf022b4cd1043d27a0f0b68a82c51942d8
Mercedes Coyle <me@mercedescoyle.com>
2018-04-30 00:34:55 +0000

    Added python script for door open/close
    Using Raspberry Pi GPIO pins instead of Arduino for controlling
    the motor and switches to open/close the door.

---
+++ doorbot/door_script.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as gpio
+
+def setup():
+    gpio.setmode(gpio.BOARD)
+    mode = gpio.getmode()
+    print "Mode is: ", mode
+
+    out_chan_list = [24, 26]
+    in_chan_list = [18, 22] 
+    gpio.setup(out_chan_list, gpio.OUT)
+    gpio.setup(in_chan_list, gpio.IN)
+    door_state = ""
+
+
+
+
+# pin 24 is connected to input B on the motor board.
+def open():
+    while door_state == "CLOSED":
+        gpio.output(24, gpio.HIGH)
+    gpio.output(24, gpio.LOW)
+
+
+# pin 26 is connected to input A on the motor board.
+def close():
+    gpio.output(26, gpio.HIGH)
+
+# need to debounce the switches
+def check_switch_states():
+    top_read_data = gpio.input(22)
+    bottom_read_data = gpio.input(18)
+    print "top switch: {}".format(top_read_data)
+    print "bottom switch: {}".format(bottom_read_data)
+    if top_read_data == 0 and bottom_read_data == 1:
+        door_state = "OPEN"
+    elif top_read_data == 1 and bottom_read_data == 0:
+        door_state = "CLOSED"
+
+def loop_read():
+    while True:
+        check_switch_states()
+
+
+def shutdown():
+    gpio.cleanup()
