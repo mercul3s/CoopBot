523f897e058872cfaa64839ae67463ea3bf53539
Mercedes Coyle <mercedes@sensu.io>
2018-04-29 19:09:46 -0700

    Use RPIO instead of RPi
    To take advantage of GPIO interrupts

--- doorbot/door_script.py
+++ doorbot/door_script.py
@@ -1,34 +1,33 @@
-import RPi.GPIO as gpio
+import RPIO as rpio
 
 def setup():
-    gpio.setmode(gpio.BOARD)
-    mode = gpio.getmode()
+    rpio.setmode(rpio.BOARD)
+    mode = rpio.getmode()
     print "Mode is: ", mode
 
     out_chan_list = [24, 26]
     in_chan_list = [18, 22] 
-    gpio.setup(out_chan_list, gpio.OUT)
-    gpio.setup(in_chan_list, gpio.IN)
+    rpio.setup(out_chan_list, rpio.OUT)
+    rpio.setup(in_chan_list, rpio.IN)
     door_state = ""
 
 
-
-
 # pin 24 is connected to input B on the motor board.
+# maybe use wait_for_interrupts or add_interrupt_callback here 
 def open():
     while door_state == "CLOSED":
-        gpio.output(24, gpio.HIGH)
-    gpio.output(24, gpio.LOW)
+        rpio.output(24, rpio.HIGH)
+    rpio.output(24, rpio.LOW)
 
 
 # pin 26 is connected to input A on the motor board.
 def close():
-    gpio.output(26, gpio.HIGH)
+    rpio.output(26, rpio.HIGH)
 
 # need to debounce the switches
 def check_switch_states():
-    top_read_data = gpio.input(22)
-    bottom_read_data = gpio.input(18)
+    top_read_data = rpio.input(22)
+    bottom_read_data = rpio.input(18)
     print "top switch: {}".format(top_read_data)
     print "bottom switch: {}".format(bottom_read_data)
     if top_read_data == 0 and bottom_read_data == 1:
@@ -37,10 +36,11 @@ def check_switch_states():
         door_state = "CLOSED"
 
 def debounce_reed_switch(pin_num):
-    switch_val1 = gpio.input(pin_num)
-    time.sleep(.01)
-    switch_val2 = gpio.input(pin_num)
+    switch_val1 = rpio.input(pin_num)
+    time.sleep(.01)     # sleep 10 milliseconds
+    switch_val2 = rpio.input(pin_num)
     if switch_val1 == switch_val2:
+        print "switch values equal"
             
 
 
@@ -50,4 +50,4 @@ def loop_read():
 
 
 def shutdown():
-    gpio.cleanup()
+    rpio.cleanup()
