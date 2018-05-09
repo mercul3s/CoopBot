a3d808219bb96599b20b0cb080bbc2ea1e30b650
Mercedes Coyle <me@mercedescoyle.com>
2018-05-09 06:12:06 +0000

    So close to getting door working with events

--- doorbot/door_script.py
+++ doorbot/door_script.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as gpio
+import time
 
 def setup():
     gpio.setmode(gpio.BOARD)
@@ -8,8 +9,10 @@ def setup():
     out_chan_list = [24, 26]
     in_chan_list = [18, 22] 
     gpio.setup(out_chan_list, gpio.OUT)
-    gpio.setup(in_chan_list, gpio.IN)
+    gpio.setup(in_chan_list, gpio.IN, pull_up_down=gpio.PUD_UP)
     door_state = ""
+    gpio.add_event_detect(18, gpio.FALLING, callback=stop_motor_top, bouncetime=300)
+    gpio.add_event_detect(22, gpio.FALLING, callback=stop_motor_bottom, bouncetime=300)
 
 
 # pin 24 is connected to input B on the motor board.
@@ -19,38 +22,42 @@ def setup():
 # http://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3
 
 def open():
-    while door_state == "CLOSED":
-        gpio.output(24, gpio.HIGH)
+    print "opening door"
+    gpio.output(24, gpio.HIGH)
+
+def stop_motor_top(switch_pin):
+    print "stopping motor top {}".format(switch_pin)
+    check_switch_states()
     gpio.output(24, gpio.LOW)
 
+def stop_motor_bottom(switch_pin):
+    print "stopping motor bottom {}".format(switch_pin)
+    check_switch_states()
+    gpio.output(26, gpio.LOW)
 
 # pin 26 is connected to input A on the motor board.
 def close():
+    print "closing door"
     gpio.output(26, gpio.HIGH)
+
+def callback_function_print(input_pin):
+    print "input on pin", input_pin
 
 # need to debounce the switches
 def check_switch_states():
-    top_read_data = gpio.input(22)
-    bottom_read_data = gpio.input(18)
+    top_read_data = gpio.input(18)
+    bottom_read_data = gpio.input(22)
     print "top switch: {}".format(top_read_data)
     print "bottom switch: {}".format(bottom_read_data)
-    if top_read_data == 0 and bottom_read_data == 1:
-        door_state = "OPEN"
-    elif top_read_data == 1 and bottom_read_data == 0:
-        door_state = "CLOSED"
-
-def debounce_reed_switch(pin_num):
-    switch_val1 = gpio.input(pin_num)
-    time.sleep(.01)     # sleep 10 milliseconds
-    switch_val2 = gpio.input(pin_num)
-    if switch_val1 == switch_val2:
-        print "switch values equal"
-            
-
+    #if top_read_data == 0 and bottom_read_data == 1:
+    #    door_state = "OPEN"
+    #elif top_read_data == 1 and bottom_read_data == 0:
+    #    door_state = "CLOSED"
 
 def loop_read():
-    while True:
+    for i in range(1,10):
         check_switch_states()
+        time.sleep(1)
 
 
 def shutdown():
